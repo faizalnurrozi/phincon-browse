59093a1f5f5ebe301f25f806e7c1c1d2bc0625d7
Faizal Nur Rozi <faizal.shoesmart@gmail.com>
2021-07-17 12:52:32 +0700

    Tidy script in general

--- server/http/bootstrap/router.go
+++ server/http/bootstrap/router.go
@@ -1,9 +1,9 @@
 package bootstrap
 
 import (
-	"github.com/gofiber/fiber/v2"
 	"github.com/faizalnurrozi/phincon-browse/server/http/bootstrap/routers/v1"
 	"github.com/faizalnurrozi/phincon-browse/server/http/handlers"
+	"github.com/gofiber/fiber/v2"
 )
 
 func (boot Bootstrap) RegisterRoute() {
@@ -16,7 +16,7 @@ func (boot Bootstrap) RegisterRoute() {
 	}
 
 	// Route for check health
-	rootParentGroup := boot.App.Group("/product")
+	rootParentGroup := boot.App.Group("/phincon-browse")
 	rootParentGroup.Get("", func(ctx *fiber.Ctx) error {
 		return ctx.Status(200).JSON("Work")
 	})
@@ -27,28 +27,4 @@ func (boot Bootstrap) RegisterRoute() {
 		Handler:    handlerType,
 	}
 	v1Routers.RegisterRoute()
-
-	//// Occastion route
-	//occassionRoute := routers.OccasionRoute{RouteGroup: apiV1, Handler: handlerType}
-	//occassionRoute.RegisterRoute()
-	//
-	//// Seller Brand route
-	//sellerBrandRoute := routers.SellerBrandRoute{RouteGroup: apiV1, Handler: handlerType}
-	//sellerBrandRoute.RegisterRoute()
-	//
-	//// Gender route
-	//genderRoute := routers.GenderRoute{RouteGroup: apiV1, Handler: handlerType}
-	//genderRoute.RegisterRoute()
-	//
-	//// Seller Category route
-	//categoryRoute := routers.CategoryRoute{RouteGroup: apiV1, Handler: handlerType}
-	//categoryRoute.RegisterRoute()
-	//
-	//// Seller Group Color route
-	//groupColorRoute := routers.ColorGroupRoute{RouteGroup: apiV1, Handler: handlerType}
-	//groupColorRoute.RegisterRoute()
-	//
-	//// Item route
-	//itemRoute := routers.ItemRoute{RouteGroup: apiV1, Handler: handlerType}
-	//itemRoute.RegisterRoute()
 }
